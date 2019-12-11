97b06876ec2b0fedba745400f0bf5a960f896c65
josephsy96 <josephsy96@hotmail.com>
2019-12-10 19:48:28 -0600

    add more queries

--- data_analysis.sql
+++ data_analysis.sql
@@ -4,3 +4,9 @@ FROM employees AS e
 JOIN salaries AS s ON
 e.emp_no = s.emp_no
 GROUP BY 1, 2, 3, 4, 5;
+
+-- 2. List employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN'1986-01-01' AND '1986-12-31'
+GROUP BY 1, 2, 3;
